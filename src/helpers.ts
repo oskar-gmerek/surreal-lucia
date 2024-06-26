a170db3a78ab874879dc56e571d6a57eb2e1f35f
Oskar Gmerek <oskarg@skiff.com>
2024-06-26 02:32:56 +0100

    initial commit

---
+++ src/helpers.ts
@@ -0,0 +1,43 @@
+import type { SurrealSession, SurrealUser } from "./types.ts";
+import type { DatabaseUser as LuciaDatabaseUser, DatabaseSession as LuciaDatabaseSession } from "lucia";
+
+/**
+ * Maps a SurrealSession object to a Lucia's DatabaseSession object.
+ *
+ * @param {SurrealSession} session - The SurrealSession object to map.
+ * @return {LuciaDatabaseSession} - The mapped Lucia's DatabaseSession object.
+ */
+export function mapToLuciaDatabaseSession(
+  session: SurrealSession
+): LuciaDatabaseSession {
+  const { id, user, expires_at, ...attributes } = session
+  
+  const sessionIdString = id.id.toString()
+  const userIdString = user.id.id.toString()
+
+  return {
+    id: sessionIdString,
+    expiresAt: expires_at,
+    userId: userIdString,
+    attributes
+  };
+}
+
+/**
+ * Maps a SurrealUser object to a Lucia's DatabaseUser object.
+ *
+ * @param {SurrealUser} user - The SurrealUser object to map.
+ * @return {LuciaDatabaseUser} - The mapped Lucia's DatabaseUser object.
+ */
+export function mapToLuciaDatabaseUser(
+  user: SurrealUser
+): LuciaDatabaseUser {
+  const { id, ...attributes } = user;
+  const userIdString = id.id.toString()
+
+  return {
+    id: userIdString,
+    attributes,
+  };
+}
+
---
+++ src/types.ts
@@ -0,0 +1,32 @@
+import type { RegisteredDatabaseSessionAttributes, RegisteredDatabaseUserAttributes } from "lucia";
+import type { RecordId } from "surrealdb.js";
+
+/**
+ * Maps a SurrealDB tables uses for storing sessions and users to the adapter.
+ */
+export type DatabaseTables = {
+  /** The name of the table used to store sessions in SurrealDB. */
+  sessionTableName: string,
+  /** The name of the table used to store users in SurrealDB. */
+  userTableName: string
+}
+
+/**
+ * Represents a user in the SurrealDB.
+*/
+export type SurrealUser = {
+  /** The RecordId of the user */
+  id: RecordId;
+} & RegisteredDatabaseUserAttributes;
+
+/**
+ *  Represents a session in the SurrealDB.
+ */
+export type SurrealSession = {
+  /** The RecordId of the session */
+  id: RecordId;
+  /** The expiration Date of the session. */
+  expires_at: Date;
+  /** The object representing the user that owns the session. */
+  user: SurrealUser;
+} & RegisteredDatabaseSessionAttributes;
